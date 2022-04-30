01a3a2db942a6f2b32781d2dda8649416e652c3f
cdr311 <w7r3ljyj@anonaddy.me>
2022-04-30 11:58:07 +0200

    Füge Basis-Entwicklungsumgebung hinzu

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <title>Example</title>
+  </head>
+  <body>
+    <h1>Hello, World!</h1>
+    <p>Die aktuelle MariaDB-Version ist:</p>
+    <?php
+    $pdo = new PDO('mysql:dbname=webdev;host=mysql', 'webdev', 'secret', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+    $query = $pdo->query('SHOW VARIABLES like "version"');
+
+    $row = $query->fetch();
+
+    echo 'MySQL version: ' . $row['Value'];
+    ?>
+  </body>
+</html>

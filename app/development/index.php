76b5ee63855b3af232a97957b88c483eef443f67
cdr311 <w7r3ljyj@anonaddy.me>
2022-05-10 20:42:36 +0200

    Benutze die Umgebungsvariablen
    Es werden nun anstatt der hartkodierten Parameter die gesetzten Umgebungsvariablen genutzt. Dies gestaltet den Code flexibler.

--- app/development/index.php
+++ app/development/index.php
@@ -8,7 +8,7 @@
     <h1>Hello, World!</h1>
     <p>Die aktuelle MariaDB-Version ist:</p>
     <?php
-    $pdo = new PDO('mysql:dbname=survey;host=mysql', 'webdev', 'secret', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    $pdo = new PDO('mysql:host=' . $_ENV['MYSQL_HOST'] . ';dbname=' . $_ENV['MYSQL_DATABASE'], $_ENV['MYSQL_USER'], $_ENV['MYSQL_PASSWORD'], [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
     $query = $pdo->query('SHOW VARIABLES like "version"');
 

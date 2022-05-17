1a5b7071b864d1208b9bef871fea6ef6c30a9a57
MSeitz <vi-mseitz@hof-university.de>
2022-05-18 00:00:38 +0200

    Aenderungen an Einführungpage und Abschlusspage der Umfrage

--- app/development/umfragescript.js
+++ app/development/umfragescript.js
@@ -1,8 +1,8 @@
 function cookieOk() {
   var now = new Date(); // Variable für aktuelles Datum
   var lifetime = now.getTime(); // Variable für Millisekunden seit 1970 bis aktuelles Datum
-  // var deleteCookie = lifetime + 2592000000; // Macht den Cookie 30 Tage gültig.
-  var deleteCookie = lifetime + 2;
+  var deleteCookie = lifetime + 2592000000; // Macht den Cookie 30 Tage gültig.
+  // var deleteCookie = lifetime + 2;
 
   now.setTime(deleteCookie);
   var enddate = now.toUTCString();

69f06f2ea003a2207974179722986c955254a99d
MSeitz <vi-mseitz@hof-university.de>
2022-05-29 11:54:22 +0200

    Umwandlung in PHP, Anpassungen an Design und erster Entwurf zur Cookie Abfrage

---
+++ app/development/umfragepagephp2.php
@@ -0,0 +1,304 @@
+<!DOCTYPE html>
+<html lang="de" dir="ltr">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="author" content="Matthias Seitz" />
+  <meta name="keywords" content="Umfrage, Digitale Nutzung" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="description" content="Online Umfrage zur Nutzung und Verwendung von digitalen Geräten" />
+
+  <link rel="stylesheet" href="umfragestyle2.css" />
+  <link rel="icon" type="image/x-icon" href="images/fragezeichen.png" />
+
+  <title>Umfrage zur Nutzung digitaler Geräte</title>
+
+</head>
+
+<body id="aussen">
+
+  <?php
+if( isset($_COOKIE['setCookieHinweis']) ) {
+  $showPopup = false;
+} else {
+  $showPopup = true;
+}
+?>
+
+<?php if($showPopup) { ?>
+<div id="cookie-popup">
+  <div class="hinweis">
+    <p>Wir verwenden Cookies. Durch die weitere Nutzung der Webseite stimmen Sie der Verwendung von Cookies zu.</p>
+  </div>
+  <span class="more">
+    <a href="https://www.gesetze-bayern.de/Content/Document/BayDSG/" id="abstand1">Details</a>
+  </span>
+  <button onclick='cookieOk()' id="abstand2">Einverstanden</button>
+</div>
+<?php  }; ?>
+
+  <!--die obere Auswahlleiste-->
+  <header>
+    <article>
+      <section class="links">
+        <div>
+          <div><img src="images/statistik.png" alt="Statistikbild" width="110px" height="50px" title="iStock" /></div>
+      </section>
+      <section class="mitte">
+        <div class="ueberschrift">Umfrage</div>
+
+      </section>
+      <section class="rechts">
+        <table>
+          <th><a href="umfragepagephp1.php" title="Umfrage" class="unter"><span>Umfrage</span></a></th>
+          <th><a href="infospagephp.php" title="Über Uns" class="unter"><span>Infos</span></a></th>
+          <th><a href="linkpagephp.php" title="Links" class="unter"><span>Links</span></a></th>
+          <th><a href="startpagephp.php" title="Home" class="unter"><span>Home</span></a></th>
+          <th>
+            <button onclick="circleklick()" title="Moduswechsel">M</button>
+          </th>
+        </table>
+      </section>
+    </article>
+  </header>
+
+  <!-- im Menü dann die übersicht machen welche thematik und die Fragen dann darunter mit DropDown bzw Liste darunter -->
+  <menu id="gross">
+    <div class="sidenav">
+      <p class="gliederung" id="kasten"><b>Übersicht:</b></p>
+      <ol type="1">
+        <li id="kasten"><button type="button" name="button" onclick="aufklappenA()" class="dropdown drueber" id="kasten">Allgemein</button></li>
+        <ul id="infoAllgemein" class="ausklappbar">
+          <li class="drop"><span class="droppen">Geschlecht</span></li>
+          <li class="drop"><span class="droppen">Alter</span></li>
+          <li class="drop"><span class="droppen">Beruf</span></li>
+          <li class="drop"><span class="droppen">Familienstand</span></li>
+          <li class="drop"><span class="droppen">Bildung</span></li>
+        </ul>
+        <li id="kasten"><button type="button" name="button" onclick="aufklappenG()" class="dropdown drueber" id="kasten">Gerätenutzung</button></li>
+        <ul id="infoGerat" class="ausklappbar">
+          <li class="drop"><span class="droppen">Geräte</span></li>
+          <li class="drop"><span class="droppen">Zweck&Nutzung</span></li>
+        </ul>
+        <li id="kasten"><button type="button" name="button" onclick="aufklappenSo()" class="dropdown drueber" id="kasten">Software</button></li>
+        <ul id="infoSoftware" class="ausklappbar">
+          <li class="drop"><span class="droppen">Betriebssystem</span></li>
+          <li class="drop"><span class="droppen">Browser</span></li>
+        </ul>
+        <li id="kasten"><button type="button" name="button" onclick="aufklappenSi()" class="dropdown drueber" id="kasten">Sicherheit</button></li>
+        <ul id="infoSicherheit" class="ausklappbar">
+          <li class="drop"><span class="droppen">E-Mail</span></li>
+          <li class="drop"><span class="droppen">Verschlüsselung</span></li>
+        </ul>
+        <li id="kasten"><button type="button" name="button" onclick="aufklappenF()" class="dropdown drueber" id="kasten">Fazit</button></li>
+        <ul id="infoFazit" class="ausklappbar">
+          <li class="drop"><span class="droppen">Bewertung</span></li>
+          <li class="drop"><span class="droppen">Feedback</span></li>
+        </ul>
+      </ol>
+    </div>
+  </menu>
+
+  <nav id="klein">
+    <span class="kleiner" onclick="openNav()">&#9776;</span>
+  </nav>
+
+  <aside id="sidebar">
+    <div class="sidenav2">
+      <p><button onclick="closeNav()" class="Xrechts"><b>X</b></button></p>
+      <!-- <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a> -->
+      <p class="gliederung" id="kasten"><b>Übersicht:</b></p>
+      <ol type="1">
+        <li id="kasten"><button type="button" name="button" onclick="aufklappenAA()" class="dropdown drueber" id="kasten">Allgemein</button></li>
+        <ul id="infoAllgemeinA" class="ausklappbar">
+          <li class="drop"><span class="droppen">Geschlecht</span></li>
+          <li class="drop"><span class="droppen">Alter</span></li>
+          <li class="drop"><span class="droppen">Beruf</span></li>
+          <li class="drop"><span class="droppen">Familienstand</span></li>
+          <li class="drop"><span class="droppen">Bildung</span></li>
+        </ul>
+        <li id="kasten"><button type="button" name="button" onclick="aufklappenGG()" class="dropdown drueber" id="kasten">Gerätenutzung</button></li>
+        <ul id="infoGeratG" class="ausklappbar">
+          <li class="drop"><span class="droppen">Geräte</span></li>
+          <li class="drop"><span class="droppen">Zweck&Nutzung</span></li>
+        </ul>
+        <li id="kasten"><button type="button" name="button" onclick="aufklappenSoSo()" class="dropdown drueber" id="kasten">Software</button></li>
+        <ul id="infoSoftwareSo" class="ausklappbar">
+          <li class="drop"><span class="droppen">Betriebssystem</span></li>
+          <li class="drop"><span class="droppen">Browser</span></li>
+        </ul>
+        <li id="kasten"><button type="button" name="button" onclick="aufklappenSiSi()" class="dropdown drueber" id="kasten">Sicherheit</button></li>
+        <ul id="infoSicherheitSi" class="ausklappbar">
+          <li class="drop"><span class="droppen">E-Mail</span></li>
+          <li class="drop"><span class="droppen">Verschlüsselung</span></li>
+        </ul>
+        <li id="kasten"><button type="button" name="button" onclick="aufklappenFF()" class="dropdown drueber" id="kasten">Fazit</button></li>
+        <ul id="infoFazitF" class="ausklappbar">
+          <li class="drop"><span class="droppen">Bewertung</span></li>
+          <li class="drop"><span class="droppen">Feedback</span></li>
+        </ul>
+      </ol>
+    </div>
+  </aside>
+
+  <main>
+    <div class="main">
+      <div class="auswahl1">
+        <h2>Welche der folgenden Geräte haben Sie schon benutzt?</h2>
+        <section>
+          <input type="checkbox" name="gerat" id="pc" value="Desktop" />
+          <label for="pc">Desktop PC</label><br />
+          <input type="checkbox" name="gerat" id="laptop" value="Laptop" />
+          <label for="laptop">Laptop</label><br />
+          <input type="checkbox" name="gerat" id="smartp" value="Smartphone" />
+          <label for="smartp">Smartphone</label><br />
+          <input type="checkbox" name="gerat" id="tab" value="Tablet" />
+          <label for="tab">Tablet</label><br />
+          <input type="checkbox" name="gerat" id="smartt" value="SmartTV" />
+          <label for="smartt">Smart TV</label><br />
+          <input type="checkbox" name="gerat" id="smartw" value="Smartwatch" />
+          <label for="smartw">Smartwatch</label><br />
+          <input type="checkbox" name="gerat" id="spiel" value="Spielekonsole" />
+          <label for="spiel">Spielekonsole</label><br />
+        </section>
+      </div>
+      <div class="auswahl2">
+        <h2>Für welche Tätigkeiten verwenden Sie ihren Desktop PC?</h2>
+        <section>
+          <input type="checkbox" name="gerat" id="arbeit" value="Arbeit" />
+          <label for="arbeit">Arbeit</label><br />
+          <input type="checkbox" name="gerat" id="surfen" value="Surfen" />
+          <label for="surfen">Surfen im Internet</label><br />
+          <input type="checkbox" name="gerat" id="shop" value="Shopping" />
+          <label for="shop">Online Shopping</label><br />
+          <input type="checkbox" name="gerat" id="film" value="Filme" />
+          <label for="film">Videos/Filme/Serien schauen</label><br />
+          <input type="checkbox" name="gerat" id="spiele" value="Spiele" />
+          <label for="spiele">Spiele spielen</label><br />
+          <label for="anderes">Andere Verwendungszwecke:</label>
+          <input type="text" name="gerat" id="anderes" />
+        </section>
+      </div>
+      <div class="auswahl3">
+        <h2>Für welche Tätigkeiten verwenden Sie ihren Laptop?</h2>
+        <section>
+          <input type="checkbox" name="gerat" id="arbeit" value="Arbeit" />
+          <label for="arbeit">Arbeit</label><br />
+          <input type="checkbox" name="gerat" id="surfen" value="Surfen" />
+          <label for="surfen">Surfen im Internet</label><br />
+          <input type="checkbox" name="gerat" id="shop" value="Shopping" />
+          <label for="shop">Online Shopping</label><br />
+          <input type="checkbox" name="gerat" id="film" value="Filme" />
+          <label for="film">Videos/Filme/Serien schauen</label><br />
+          <input type="checkbox" name="gerat" id="spiele" value="Spiele" />
+          <label for="spiele">Spiele spielen</label><br />
+          <label for="anderes">Andere Verwendungszwecke:</label>
+          <input type="text" name="gerat" id="anderes" />
+        </section>
+      </div>
+      <div class="auswahl4">
+        <h2>Für welche Tätigkeiten verwenden Sie ihr Smartphone?</h2>
+        <section>
+          <input type="checkbox" name="gerat" id="arbeit" value="Arbeit" />
+          <label for="arbeit">Arbeit</label><br />
+          <input type="checkbox" name="gerat" id="surfen" value="Surfen" />
+          <label for="surfen">Surfen im Internet</label><br />
+          <input type="checkbox" name="gerat" id="shop" value="Shopping" />
+          <label for="shop">Online Shopping</label><br />
+          <input type="checkbox" name="gerat" id="film" value="Filme" />
+          <label for="film">Videos/Filme/Serien schauen</label><br />
+          <input type="checkbox" name="gerat" id="spiele" value="Spiele" />
+          <label for="spiele">Spiele spielen</label><br />
+          <label for="anderes">Andere Verwendungszwecke:</label>
+          <input type="text" name="gerat" id="anderes" />
+        </section>
+      </div>
+      <div class="auswahl5">
+        <h2>Für welche Tätigkeiten verwenden Sie ihr Tablet?</h2>
+        <section>
+          <input type="checkbox" name="gerat" id="arbeit" value="Arbeit" />
+          <label for="arbeit">Arbeit</label><br />
+          <input type="checkbox" name="gerat" id="surfen" value="Surfen" />
+          <label for="surfen">Surfen im Internet</label><br />
+          <input type="checkbox" name="gerat" id="shop" value="Shopping" />
+          <label for="shop">Online Shopping</label><br />
+          <input type="checkbox" name="gerat" id="film" value="Filme" />
+          <label for="film">Videos/Filme/Serien schauen</label><br />
+          <input type="checkbox" name="gerat" id="spiele" value="Spiele" />
+          <label for="spiele">Spiele spielen</label><br />
+          <label for="anderes">Andere Verwendungszwecke:</label>
+          <input type="text" name="gerat" id="anderes" />
+        </section>
+      </div>
+      <div class="auswahl6">
+        <h2>Für welche Tätigkeiten verwenden Sie ihren Smart TV?</h2>
+        <section>
+          <input type="checkbox" name="gerat" id="arbeit" value="Arbeit" />
+          <label for="arbeit">Arbeit</label><br />
+          <input type="checkbox" name="gerat" id="surfen" value="Surfen" />
+          <label for="surfen">Surfen im Internet</label><br />
+          <input type="checkbox" name="gerat" id="shop" value="Shopping" />
+          <label for="shop">Online Shopping</label><br />
+          <input type="checkbox" name="gerat" id="film" value="Filme" />
+          <label for="film">Videos/Filme/Serien schauen</label><br />
+          <input type="checkbox" name="gerat" id="spiele" value="Spiele" />
+          <label for="spiele">Spiele spielen</label><br />
+          <label for="anderes">Andere Verwendungszwecke:</label>
+          <input type="text" name="gerat" id="anderes" />
+        </section>
+      </div>
+      <div class="auswahl7">
+        <h2>Für welche Tätigkeiten verwenden Sie ihre Smartwatch?</h2>
+        <section>
+          <input type="checkbox" name="gerat" id="arbeit" value="Arbeit" />
+          <label for="arbeit">Arbeit</label><br />
+          <input type="checkbox" name="gerat" id="surfen" value="Surfen" />
+          <label for="surfen">Surfen im Internet</label><br />
+          <input type="checkbox" name="gerat" id="shop" value="Shopping" />
+          <label for="shop">Online Shopping</label><br />
+          <input type="checkbox" name="gerat" id="film" value="Filme" />
+          <label for="film">Videos/Filme/Serien schauen</label><br />
+          <input type="checkbox" name="gerat" id="spiele" value="Spiele" />
+          <label for="spiele">Spiele spielen</label><br />
+          <input type="checkbox" name="gerat" id="gesund" value="Gesundheit" />
+          <label for="gesund">Körperwerte kontollieren</label><br />
+          <label for="anderes">Andere Verwendungszwecke:</label>
+          <input type="text" name="gerat" id="anderes" />
+        </section>
+      </div>
+      <div class="auswahl8">
+        <h2>Für welche Tätigkeiten verwenden Sie ihre Spielekonsole?</h2>
+        <section>
+          <input type="checkbox" name="gerat" id="arbeit" value="Arbeit" />
+          <label for="arbeit">Arbeit</label><br />
+          <input type="checkbox" name="gerat" id="surfen" value="Surfen" />
+          <label for="surfen">Surfen im Internet</label><br />
+          <input type="checkbox" name="gerat" id="shop" value="Shopping" />
+          <label for="shop">Online Shopping</label><br />
+          <input type="checkbox" name="gerat" id="film" value="Filme" />
+          <label for="film">Videos/Filme/Serien schauen</label><br />
+          <input type="checkbox" name="gerat" id="spiele" value="Spiele" />
+          <label for="spiele">Spiele spielen</label><br />
+          <label for="anderes">Andere Verwendungszwecke:</label>
+          <input type="text" name="gerat" id="anderes" />
+        </section>
+      </div>
+      <div class="vorher">
+        <a href="umfragepagephp1.php" title="Vorherige Seite"><button type="button" name="umfrage">Vorherige Seite</button></a>
+      </div>
+      <div class="naechste">
+        <a href="umfragepagephp3.php" title="Folgende Seite"><button type="button" name="umfrage">Nächste Seite</button></a>
+      </div>
+    </div>
+  </main>
+
+  <footer class="mittig">
+    <p>Die Gesamte Umfrage wurde von Alexander Eigler, Jonas Kraus und Matthias Seitz entworfen.</p>
+    <p>Images werden von <a class="quelle" href="https://www.istockphoto.com/de" title="iStock">iStock</a>verwendet.</p>
+    <p>copyright@2022</p>
+  </footer>
+  <script type="text/javascript" src="umfragescript.js">
+  </script>
+</body>
+
+</html>
--- app/development/umfragescript.js
+++ app/development/umfragescript.js
@@ -16,12 +16,12 @@ function circleklick() {
   element.classList.toggle("darkmode")
 }
 
-document.getElementById('aussen').style.opacity = '0.5'
-window.addEventListener('load', () =>{
-  if(confirm("Unsere Website verwendet Cookies um ausgewählte Daten über Sie zu sammeln. Bevor Sie fortfahren, unsere Website zu nutzen, müssen Sie den Datenschutzbestimmungen zustimmen"))
-      document.getElementById('aussen').style.opacity = '1.0';
-  document.getElementById('aussen').style.opacity = '1.0';
-})
+// document.getElementById('aussen').style.opacity = '0.5'
+// window.addEventListener('load', () =>{
+//   if(confirm("Unsere Website verwendet Cookies um ausgewählte Daten über Sie zu sammeln. Bevor Sie fortfahren, unsere Website zu nutzen, müssen Sie den Datenschutzbestimmungen zustimmen"))
+//       document.getElementById('aussen').style.opacity = '1.0';
+//   document.getElementById('aussen').style.opacity = '1.0';
+// })
 
 var gedruecktA = false;
 function aufklappenA(){

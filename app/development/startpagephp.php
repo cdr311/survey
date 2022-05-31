bd9df9690799f1578e26f71c267c90493f83153c
cdr311 <w7r3ljyj@anonaddy.me>
2022-05-31 21:24:55 +0200

    Revert "Umwandlung in PHP, Anpassungen an Design und erster Entwurf zur Cookie Abfrage"
    This reverts commit 69f06f2ea003a2207974179722986c955254a99d.

--- app/development/startpagephp.php
+++ app/development/startpagephp.php
@@ -9,102 +9,76 @@
   <meta name="description" content="Online Umfrage zur Nutzung und Verwendung von digitalen Geräten" />
 
   <link rel="stylesheet" href="startpagestyle.css" />
-  <link rel="icon" type="image/x-icon" href="images/fragezeichen.png" />
+  <link rel="icon" type="image/x-icon" href="https://media.istockphoto.com/vectors/question-mark-icon-flat-vector-illustration-design-vector-id1162198273" />
 
   <title>Umfrage zur Nutzung digitaler Geräte</title>
 
 </head>
 
-<body id="aussen">
-
+<body>
   <?php
-if( isset($_COOKIE['setCookieHinweis']) ) {
-  $showPopup = false;
-} else {
-  $showPopup = true;
-}
+  if( isset($_COOKIE['setCookieHinweis']) ) {
+    $showPopup = false;
+  } else {
+    $showPopup = true;
+  }
 ?>
 
 <?php if($showPopup) { ?>
-<div id="cookie-popup">
-  <div class="hinweis">
-    <p>Wir verwenden Cookies. Durch die weitere Nutzung der Webseite stimmen Sie der Verwendung von Cookies zu.</p>
+  <div id="cookie-popup">
+    <div class="hinweis">
+      <p>Wir verwenden Cookies. Durch die weitere Nutzung der Webseite stimmen Sie der Verwendung von Cookies zu.</p>
+    </div>
+    <span class="more">
+      <a href="https://www.gesetze-bayern.de/Content/Document/BayDSG/" id="abstand1">Details</a>
+    </span>
+    <button onclick='cookieOk()' id="abstand2">Einverstanden</button>
   </div>
-  <span class="more">
-    <a href="https://www.gesetze-bayern.de/Content/Document/BayDSG/" id="abstand1">Details</a>
-  </span>
-  <button onclick='cookieOk()' id="abstand2">Einverstanden</button>
-</div>
 <?php  }; ?>
 
   <!--die obere Auswahlleiste-->
   <header>
-    <article>
-      <section class="links">
-        <div><img src="images/statistik.png" alt="Statistikbild" width="110px" height="50px" title="iStock" /></div>
-      </section>
-      <section class="mitte">
-        <div class="ueberschrift">Umfrage</div>
+    <section class="links">
+      <div>
+        <div><img src="https://media.istockphoto.com/photos/stock-market-investment-graph-with-indicator-and-volume-data-picture-id1221293664" alt="Statistikbild" width="110px" height="50px" title="iStock" /></div>
+    </section>
+    <section class="mitte">
+      <div class="ueberschrift">Umfrage</div>
 
-      </section>
-      <section class="rechts">
-        <table>
-          <th><a href="umfragepagephp1.php" title="Umfrage" class="unter"><span>Umfrage</span></a></th>
-          <th><a href="infospagephp.php" title="Über Uns" class="unter"><span>Infos</span></a></th>
-          <th><a href="linkpagephp.php" title="Links" class="unter"><span>Links</span></a></th>
-          <th><a href="startpagephp.php" title="Home" class="unter"><span>Home</span></a></th>
-          <th>
-            <button onclick="circleklick()" title="Moduswechsel">M</button>
-          </th>
-        </table>
+    </section>
+    <section class="rechts">
+      <table>
+        <th><a href="" title="Umfrage" class="unter"><span>Umfrage</span></a></th>
+        <th><a href="infospage.html" title="Über Uns" class="unter"><span>Infos</span></a></th>
+        <th><a href="linkpage.html" title="Links" class="unter"><span>Links</span></a></th>
+        <th><a href="startpage.html" title="Home" class="unter"><span>Home</span></a></th>
+        <th><a href="" title="Moduswechsel" class="unter">
+            <svg width="30" height="30">
+              <circle cx="15" cy="15" r="11" fill="black" />
+              Sorry, your browser does not support inline SVG.
+            </svg> </a>
+        </th>
+      </table>
 
-      </section>
-    </article>
+    </section>
 
   </header>
 
+  <!--der linke Seitenkasten-->
   <nav>
-    <div class="bild-inhalt">
-      <img class="meineSlides" src="images/umfrage.png" alt="UmfrageBild1" />
-      <img class="meineSlides" src="images/fragen.png" alt="UmfrageBild2" />
-      <img class="meineSlides" src="images/smiley.png" alt="UmfrageBild3" />
-      <img class="meineSlides" src="images/holz.png" alt="UmfrageBild4" />
-      <img class="meineSlides" src="images/dunkel.png" alt="UmfrageBild5" />
-      <h1 class="linksunten">Nutzung digitaler Geräte</h1>
-    </div>
 
   </nav>
 
+  <div>
+
+  </div>
+
   <main>
-    <button class="zugeordnet">Um was geht es?</button>
-    <div class="panel">
-      <p>In dieser Umfrage werden insgesamt - je nach Auswahl - circa 10 bis 30 Fragen zum Thema: <b>Nutzung und Verwendung von digitalen Geräten</b> auf Sie zukommen.</p>
-      <p>Dabei geht es von den verschiedenen Geräten, die von Ihnen in Nutzung sind, über den Zweck, bis hin zur Nutzungszeit.</p>
-      <p>Des Weiteren erwarten Sie Fragen zur Nutzung von ausgewählen Programmen/Softwaren.</p>
-    </div>
 
-    <button class="zugeordnet">Warum sollten Sie teilnehmen?</button>
-    <div class="panel">
-      <ul>
-        <li>Sie sollten Teilnehmen, da Sie selbst danach einen besseren Überblick haben, welche digitalen Geräten bei Ihnen im Einsatz sind.</li>
-        <li>Sie helfen den Entwicklern der Umfrage, um eine bessere Auswertung erstellen zu können.</li>
-        <li>Diese Umfrage hat eine geringe Bearbeitungsdauer.</li>
-      </ul>
-    </div>
-
-    <button class="zugeordnet">Wer will das wissen?</button>
-    <div class="panel">
-      <p>Wenn Sie genauere Details über die Entwickler erfahren wollen, dann schauen Sie beim Info Tab vorbei oder klicken Sie <a href="infospage.html" title="Über Uns" alt="Infopage">hier</a>.</p>
-    </div>
-    <button class="zugeordnet">Ist die Umfrage anonym?</button>
-    <div class="panel">
-      <p>Die Umfrage ist vollständig anonym. Es werden lediglich 4 allgemeine Fragen zu Beginn auftauchen, welche aber durch den Button "<em>Keine Angabe</em>" übersprungen werden können. </p>
-      <p>Jedoch wäre es für die Auswertung sehr hilfreich, wenn Sie auch hier was angeben würden.</p>
-    </div>
   </main>
 
-  <aside>
-    <a href="umfragepagephp1.php" title="Umfrage starten" class="unter"><button type="button" name="umfrage" class="umfrage">Umfrage</button></a>
+  <aside class="">
+
   </aside>
 
   <footer class="mittig">
@@ -114,9 +88,7 @@ if( isset($_COOKIE['setCookieHinweis']) ) {
 
   </footer>
 
-  <script type="text/javascript" src="startpagescript.js">
-
-  </script>
+    <script type="text/javascript" src="startpagescript.js"></script>
 
 </body>
 

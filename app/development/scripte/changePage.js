5372384144f098de7771f9add2c1fee21d3401a9
ae <alexander.eigler@hof-university.de>
2022-06-12 20:05:57 +0200

    Validation für Browser, Mailprogramm, VerschluesselungPrivat, VerschluesselungDienstlich und Bewertung hinzugefuegt

--- app/development/scripte/changePage.js
+++ app/development/scripte/changePage.js
@@ -33,7 +33,7 @@ function validateLeavingPage1(){
 
   for (var i = 0; i < geschlechtRadios.length; i++) {
     if (geschlechtRadios[i].checked) {
-      geschlechtSelected = true; // checked
+      geschlechtSelected = true;
     }
   };
 
@@ -42,7 +42,7 @@ function validateLeavingPage1(){
 
   for (var i = 0; i < alterRadios.length; i++) {
     if (alterRadios[i].checked) {
-      alterSelected = true; // checked
+      alterSelected = true;
     }
   };
 
@@ -51,7 +51,7 @@ function validateLeavingPage1(){
 
   for (var i = 0; i < arbeitRadios.length; i++) {
     if (arbeitRadios[i].checked) {
-      arbeitSelected = true; // checked
+      arbeitSelected = true;
     }
   };
 
@@ -60,7 +60,7 @@ function validateLeavingPage1(){
 
   for (var i = 0; i < familieRadios.length; i++) {
     if (familieRadios[i].checked) {
-      familieSelected = true; // checked
+      familieSelected = true;
     }
   };
 
@@ -97,13 +97,83 @@ function validateLeavingPage3(){
 }
 
 function validateLeavingPage4(){
+  var browserRadios = document.getElementsByName("Browser");
+  var browserSelected = false;
+
+  for (var i = 0; i < browserRadios.length; i++) {
+    if (browserRadios[i].checked) {
+      browserSelected = true;
+    }
+  };
+
+  if(!browserSelected){
+    alert("Browser fehlt");
+    return false;
+  }
+
   return true;
 }
 
 function validateLeavingPage5(){
+  var mailprogrammCheckboxes = document.getElementsByName("mailprogramm[]");
+  var mailprogrammSelected = false;
+
+  for (var i = 0; i < mailprogrammCheckboxes.length; i++) {
+    if (mailprogrammCheckboxes[i].checked) {
+      mailprogrammSelected = true;
+    }
+  };
+
+  var verschluesselungPrivatRadios = document.getElementsByName("verschluesselungPrivat");
+  var verschluesselungPrivatSelected = false;
+
+  for (var i = 0; i < verschluesselungPrivatRadios.length; i++) {
+    if (verschluesselungPrivatRadios[i].checked) {
+      verschluesselungPrivatSelected = true;
+    }
+  };
+
+  var verschluesselungDienstlichRadios = document.getElementsByName("verschluesselungDienstlich");
+  var verschluesselungDienstlichSelected = false;
+
+  for (var i = 0; i < verschluesselungDienstlichRadios.length; i++) {
+    if (verschluesselungDienstlichRadios[i].checked) {
+      verschluesselungDienstlichSelected = true;
+    }
+  };
+
+  if(!mailprogrammSelected){
+    alert("mindestens 1 Mailprogramm auswählen");
+    return false;
+  }
+
+  if(!verschluesselungPrivatSelected){
+    alert("Verschlüsselung (Privat) fehlt");
+    return false;
+  }
+
+  if(!verschluesselungDienstlichSelected){
+    alert("Verschlüsselung (Dienstlich) fehlt");
+    return false;
+  }
+
   return true;
 }
 
 function validateLeavingPage6(){
+  var bewertungRadios = document.getElementsByName("bewertung");
+  var bewertungSelected = false;
+
+  for (var i = 0; i < bewertungRadios.length; i++) {
+    if (bewertungRadios[i].checked) {
+      bewertungSelected = true;
+    }
+  };
+
+  if(!bewertungSelected){
+    alert("Bewertung fehlt");
+    return false;
+  }
+
   return true;
 }

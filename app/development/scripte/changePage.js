b57812beecb2d402d8407c30657a36585d222000
cdr311 <w7r3ljyj@anonaddy.me>
2022-06-12 23:44:38 +0200

    Füge die Eingabevalidierung für umfrage3.php hinzu und vervollständige die Eingabevalidierung für umfrage4.php

--- app/development/scripte/changePage.js
+++ app/development/scripte/changePage.js
@@ -206,10 +206,204 @@ function validateLeavingPage2(){
 }
 
 function validateLeavingPage3(){
+  const pcRadios = document.getElementsByName("nutzungszeitDesktop");
+  const laptopRadios = document.getElementsByName("nutzungszeitLaptop");
+  const smartpRadios = document.getElementsByName("nutzungszeitSmartphone");
+  const tabRadios = document.getElementsByName("nutzungszeitTablet");
+  const smarttRadios = document.getElementsByName("nutzungszeitSmartTV");
+  const smartwRadios = document.getElementsByName("nutzungszeitSmartwatch");
+  const spielRadios = document.getElementsByName("nutzungszeitSpielekonsole");
+  let pcSelected = false;
+  let laptopSelected = false;
+  let smartpSelected = false;
+  let tabSelected = false;
+  let smarttSelected = false;
+  let smartwSelected = false;
+  let spielSelected = false;
+
+  for (let i = 0; i < pcRadios.length; i++) {
+    if (pcRadios[i].checked) {
+      pcSelected = true;
+      break;
+    }
+  }
+  
+  for (let i = 0; i < laptopRadios.length; i++) {
+    if (laptopRadios[i].checked) {
+      laptopSelected = true;
+      break;
+    }
+  }
+  
+  for (let i = 0; i < smartpRadios.length; i++) {
+    if (smartpRadios[i].checked) {
+      smartpSelected = true;
+      break;
+    }
+  }
+
+  for (let i = 0; i < tabRadios.length; i++) {
+    if (tabRadios[i].checked) {
+      tabSelected = true;
+      break;
+    }
+  }
+
+  for (let i = 0; i < smarttRadios.length; i++) {
+    if (smarttRadios[i].checked) {
+      smarttSelected = true;
+      break;
+    }
+  }
+
+  for (let i = 0; i < smartwRadios.length; i++) {
+    if (smartwRadios[i].checked) {
+      smartwSelected = true;
+      break;
+    }
+  }
+
+  for (let i = 0; i < spielRadios.length; i++) {
+    if (spielRadios[i].checked) {
+      spielSelected = true;
+      break;
+    }
+  }
+
+  if (sessionStorage.getItem("pc")) {
+    if (!pcSelected) {
+      alert("Bitte geben Sie die Nutzungsdauer für Ihren Desktop PC an.");
+      return false;
+    }
+  }
+
+  if (sessionStorage.getItem("laptop")) {
+    if (!laptopSelected) {
+      alert("Bitte geben Sie die Nutzungsdauer für Ihren Laptop an.");
+      return false;
+    }
+  }
+
+  if (sessionStorage.getItem("smartp")) {
+    if (!smartpSelected) {
+      alert("Bitte geben Sie die Nutzungsdauer für Ihr Smartphone an.");
+      return false;
+    }
+  }
+
+  if (sessionStorage.getItem("tab")) {
+    if (!tabSelected) {
+      alert("Bitte geben Sie die Nutzungsdauer für Ihr Tablet an.");
+      return false;
+    }
+  }
+
+  if (sessionStorage.getItem("smartt")) {
+    if (!smarttSelected) {
+      alert("Bitte geben Sie die Nutzungsdauer für Ihr Smart TV an.");
+      return false;
+    }
+  }
+
+  if (sessionStorage.getItem("smartw")) {
+    if (!smartwSelected) {
+      alert("Bitte geben Sie die Nutzungsdauer für Ihre Smartwatch an.");
+      return false;
+    }
+  }
+
+  if (sessionStorage.getItem("spiel")) {
+    if (!spielSelected) {
+      alert("Bitte geben Sie die Nutzungsdauer für Ihre Spielekonsole an.");
+      return false;
+    }
+  }
+
   return true;
 }
 
 function validateLeavingPage4(){
+  const pcRadios = document.getElementsByName("OSDesktop");
+  const laptopRadios = document.getElementsByName("OSLaptop");
+  const smartpRadios = document.getElementsByName("OSSmartphone");
+  const tabRadios = document.getElementsByName("OSTablet");
+  const smarttRadios = document.getElementsByName("OSSmartTV");
+  let pcSelected = false;
+  let laptopSelected = false;
+  let smartpSelected = false;
+  let tabSelected = false;
+  let smarttSelected = false;
+
+  for (let i = 0; i < pcRadios.length; i++) {
+    if (pcRadios[i].checked) {
+      pcSelected = true;
+      break;
+    }
+  }
+  
+  for (let i = 0; i < laptopRadios.length; i++) {
+    if (laptopRadios[i].checked) {
+      laptopSelected = true;
+      break;
+    }
+  }
+  
+  for (let i = 0; i < smartpRadios.length; i++) {
+    if (smartpRadios[i].checked) {
+      smartpSelected = true;
+      break;
+    }
+  }
+
+  for (let i = 0; i < tabRadios.length; i++) {
+    if (tabRadios[i].checked) {
+      tabSelected = true;
+      break;
+    }
+  }
+
+  for (let i = 0; i < smarttRadios.length; i++) {
+    if (smarttRadios[i].checked) {
+      smarttSelected = true;
+      break;
+    }
+  }
+
+  if (sessionStorage.getItem("pc")) {
+    if (!pcSelected) {
+      alert("Bitte geben Sie ein Betriebssystem für Ihren Desktop PC an.");
+      return false;
+    }
+  }
+
+  if (sessionStorage.getItem("laptop")) {
+    if (!laptopSelected) {
+      alert("Bitte geben Sie ein Betriebssystem für Ihren Laptop an.");
+      return false;
+    }
+  }
+
+  if (sessionStorage.getItem("smartp")) {
+    if (!smartpSelected) {
+      alert("Bitte geben Sie ein Betriebssystem für Ihr Smartphone an.");
+      return false;
+    }
+  }
+
+  if (sessionStorage.getItem("tab")) {
+    if (!tabSelected) {
+      alert("Bitte geben Sie ein Betriebssystem für Ihr Tablet an.");
+      return false;
+    }
+  }
+
+  if (sessionStorage.getItem("smartt")) {
+    if (!smarttSelected) {
+      alert("Bitte geben Sie ein Betriebssystem für Ihr Smart TV an.");
+      return false;
+    }
+  }
+
   var browserRadios = document.getElementsByName("Browser");
   var browserSelected = false;
 
